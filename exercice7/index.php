865068f9724add027d1c1ddae23d3053b685efb1
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2023-02-18 11:25:40 +0100

    Ajout exercice7

---
+++ exercice7/index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+ob_start();
+?>
+<h1>Objectifs</h1>
+<ul>
+    <li>Créer un formulaire d'authentification</li>
+    <li>Si l'utilisateur n'est pas connecté, afficher un lien de connexion</li>
+    <li>Si l'utilisateur n'est pas connecté, afficher un lien de création d'un nouveau compte</li>
+    <li>Si l'utilisateur est connecté, afficher un message de bienvenue</li>
+    <li>Si l'utilisateur est connecté, afficher un lien de déconnexion</li>
+    <li>Si l'utilisateur est connecté, afficher un lien de listing les utilisateurs</li>
+</ul>
+<?php if(empty($_SESSION['login'])) { ?>
+    <a href="login.php">Connexion</a>
+    <br>
+    <a href="register.php">Créer un compte</a>
+<?php } else { ?>
+    Bienvenue <?php echo $_SESSION['login']; ?> !
+    <br>
+    <a href="logout.php">Déconnexion</a>
+    <br>
+    <a href="listing.php">Liste des utilisateurs</a>
+<?php } ?>
+<?php
+/* echo '<br>'; */
+/* echo password_hash('tata', PASSWORD_DEFAULT); */
+/* echo '<br>'; */
+/* echo password_verify('tata', '$2y$10$c4xK1Y6LJJ6p9svP3CU1Y.nVz2WdvIe.30xkJ/a/hbKy7pW5ka4cu') == true; */
+/* echo '<br>'; */
+/* echo password_verify('toto', '$2y$10$c4xK1Y6LJJ6p9svP3CU1Y.nVz2WdvIe.30xkJ/a/hbKy7pW5ka4cu') == false; */
+$title = "Exercice 7";
+$content = ob_get_clean();
+include '../template.php';
+?>
